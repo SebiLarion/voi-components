ea90e1d648764b34ad74d1b184e0a628d9da75e7
Danny Waite <dannywaite7@gmail.com>
2021-01-06 19:33:45 +0000

    added test data

--- src/property/PropertyResultCard.js
+++ src/property/PropertyResultCard.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function PropertyResultCard() {
+export default function PropertyResultCard({ property }) {
   return (
     <div className="text-red-300">
-      <h1>PropertyResultCard</h1>
+      <h1>{property.name}</h1>
     </div>
   );
 }
--- src/property/PropertyResultCard.stories.js
+++ src/property/PropertyResultCard.stories.js
@@ -2,8 +2,10 @@ import React from "react";
 
 import PropertyResultCard from "./PropertyResultCard";
 
+import property from "../../data/property.json";
+
 export default {
   title: "PropertyResultCard",
 };
 
-export const Default = () => <PropertyResultCard />;
+export const Default = () => <PropertyResultCard property={property} />;
