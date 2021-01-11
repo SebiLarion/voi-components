0c3e583b1867af7c9b87033a9b9bb759cca575f1
Danny Waite <dannywaite7@gmail.com>
2021-01-11 18:33:32 +0000

    add parameters to ColorTextBlockCard

--- src/colorTextBlock/ColorTextBlock.stories.js
+++ src/colorTextBlock/ColorTextBlock.stories.js
@@ -2,10 +2,13 @@ import React from "react";
 
 import ColorTextBlockCard from "./ColorTextBlockCard";
 
-
-
 export default {
   title: "ColorTextBlockCard",
 };
 
-export const Default = () => <ColorTextBlockCard />;
+export const Default = () => (
+  <ColorTextBlockCard bgColor="bg-dark-blue">
+    When buying on the magic island, Villas of Ibiza are there to ensure the
+    result truly is magical.
+  </ColorTextBlockCard>
+);
--- src/colorTextBlock/ColorTextBlockCard.js
+++ src/colorTextBlock/ColorTextBlockCard.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-
-export default function ColorTextBlockCard() {
+export default function ColorTextBlockCard({ bgColor, children }) {
   return (
-      <div className="color-block bg-dark-blue relative">
-        
-          <div className="pt-40">
-              <div className="text-white font-libre-baskerville h-auto w-auto text-center text-2xl mx-72">When buying on the magic island, Villas of Ibiza are there to ensure the result truly is magical.</div>
-          </div>
-            <svg className="absolute fill-current text-white w-1/2 -mt-44" preserveAspectRatio="none" viewBox="0 0 100 100">
-              <polygon points="0,43 100,50 100,55 0,55"/>
-            </svg>
-            <svg className="absolute right-0 fill-current text-white w-1/2 -mt-44" preserveAspectRatio="none" viewBox="0 0 100 100">
-                <polygon points="0,50 100,43 100,55 0,55"/>
-            </svg>
-     </div>
-      
+    <div className={`color-block relative ${bgColor}`}>
+      <div className="pt-40">
+        <div className="text-white font-libre-baskerville h-auto w-auto text-center text-2xl mx-72">
+          {children}
+        </div>
+      </div>
+      <svg
+        className="absolute fill-current text-white w-1/2 -mt-44"
+        preserveAspectRatio="none"
+        viewBox="0 0 100 100"
+      >
+        <polygon points="0,43 100,50 100,55 0,55" />
+      </svg>
+      <svg
+        className="absolute right-0 fill-current text-white w-1/2 -mt-44"
+        preserveAspectRatio="none"
+        viewBox="0 0 100 100"
+      >
+        <polygon points="0,50 100,43 100,55 0,55" />
+      </svg>
+    </div>
   );
 }
